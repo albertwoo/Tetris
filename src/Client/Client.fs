3eb9b4fb325a21ca83299b7d356ec63972f50f5d
albert woo <albertwoo_slaveoftime@hotmail.com>
2018-06-17 22:20:39 +0800

    Use canvas instead

--- src/Client/Client.fs
+++ src/Client/Client.fs
@@ -9,12 +9,19 @@ open Fable.PowerPack.Fetch
 open Fable
 open Fable.Import
 open Fable.Import.JS
+open System
+open Elmish
+open Fable.Core
+open Fable.Core.JsInterop
+open Fable.Import
+open Fable.Import.Browser
 
 open Fulma
 open Fulma.FontAwesome
 
 open Shared
 open TetrisDomain
+open Fable.Import.React
 
 let defaultSpeed = 10
 let calculatSpeed score = 
@@ -32,22 +39,23 @@ type Model = {
     TimeCost: int
     Speed: int
     SpeedCount: int
-    TouchStartPos: (float * float) option
+    TouchStartPoint: (float * float) option
+    TouchMovingPoint: (float * float) option
+    TouchTime: DateTime option
     IsPaused: bool
     IsRestarting: bool
-    HideDetail: bool
-}
+    HideDetail: bool }
 
 type Msg =
-    | Action of Action
+    | Action of Action | GoBottom
     | BeginRestart | CancelRestart | Restart
     | Tick
-    | TouchStart of float * float | TouchEnd
+    | TouchStart of float * float | TouchMove of float * float | TouchEnd of float * float
     | Pause | Continue
     | HideDetail
 
 let init () =
-    let rows, columns = 28, 18
+    let rows, columns = 25, 15
     { 
         Score = 0
         Boundry = rows - 1, columns - 1
@@ -59,7 +67,9 @@ let init () =
         TimeCost = 0
         Speed = defaultSpeed
         SpeedCount = 0
-        TouchStartPos = None
+        TouchStartPoint = None
+        TouchMovingPoint = None
+        TouchTime = None
         IsPaused = false
         IsRestarting = false
         HideDetail = true
@@ -91,64 +101,135 @@ let update msg model =
                                      Score = score 
                                      Speed = calculatSpeed score
                                      SpeedCount = 0
-                                     TouchStartPos = None }, Cmd.none
+                                     TouchMovingPoint = None }, Cmd.none
                     elif not blocked then { model with MovingBlock = Some(processedMB)
                                                        PrectionBlock = 
                                                             if model.PrectionBlock.IsSome && action = Down then model.PrectionBlock
                                                             else Some(getPredictionBlock processedMB model.Boundry model.AllSquares) }, Cmd.none
                     else model, Cmd.none
         | _ -> model, Cmd.none
+    | GoBottom ->
+        match model.PrectionBlock with
+        | Some mb ->
+            let squares, lines = getScore model.Boundry (model.AllSquares @ mb.Squares)
+            let score = model.Score + lines
+            { model with PreviewBlock = Some(generateBlock (getRandomBlockType()) None |> setBlockToMid (snd model.Boundry + 1))
+                         MovingBlock = model.PreviewBlock
+                         PrectionBlock = Some(getPredictionBlock model.PreviewBlock.Value model.Boundry squares)
+                         AllSquares = squares
+                         Score = score 
+                         Speed = calculatSpeed score
+                         SpeedCount = 0
+                         TouchMovingPoint = None }, Cmd.none
+        | _ -> model, Cmd.none
     | Tick ->
-        if model.Speed <= model.SpeedCount && not model.IsPaused && not model.IsOver then
+        if model.IsOver || model.IsPaused || model.IsRestarting then
+            model, Cmd.none
+        elif model.Speed <= model.SpeedCount then
             { model with TimeCost = model.TimeCost + 1
                          SpeedCount = 0 }, Cmd.ofMsg (Down |> Msg.Action)
         else
             { model with TimeCost = model.TimeCost + 1
                          SpeedCount = model.SpeedCount + 1 }, Cmd.none
-    | TouchStart (p1, p2) -> { model with TouchStartPos = Some(p1, p2) }, Cmd.none
-    | TouchEnd -> { model with TouchStartPos = None }, Cmd.none
-    | Pause -> { model with IsPaused = true }, Cmd.none
+    | TouchStart (x, y) -> { model with TouchStartPoint = Some(x, y)
+                                        TouchMovingPoint = Some(x, y)
+                                        TouchTime = Some(DateTime.Now) }, Cmd.none
+    | TouchMove (x, y) ->
+        match model.TouchMovingPoint with
+        | Some (x0, y0) -> 
+            let dx, dy = x - x0, y - y0
+            let threshhold = 20.
+            if Math.Abs dx > Math.Abs dy && Math.Abs dx > threshhold then
+                if dx > 0. then { model with TouchMovingPoint = Some(x ,y) }, Cmd.ofMsg (Right |> Msg.Action)
+                else { model with TouchMovingPoint = Some(x ,y) },  Cmd.ofMsg (Left |> Msg.Action)
+            elif Math.Abs dx < Math.Abs dy then
+                if dy > threshhold && dy < threshhold * 2. then
+                    { model with TouchMovingPoint = Some(x ,y) }, Cmd.ofMsg (Down |> Msg.Action)
+                elif dy >= threshhold * 2. then
+                    { model with TouchMovingPoint = None }, Cmd.ofMsg GoBottom
+                else model, Cmd.none
+            else model, Cmd.none
+        | _ -> model, Cmd.none
+    | TouchEnd (x, y) ->
+        match model.TouchStartPoint with
+        | Some (x0, y0) -> 
+            let dx, dy = x - x0, y - y0
+            if Math.Abs dx < 10. && Math.Abs dy < 10. && 
+               (DateTime.Now - model.TouchTime.Value).Ticks < TimeSpan.FromMilliseconds(500.).Ticks then
+                if model.IsOver then model, Cmd.ofMsg Restart
+                else { model with TouchStartPoint = None
+                                  TouchMovingPoint = None
+                                  TouchTime = None }, Cmd.ofMsg (Rotate |> Msg.Action)
+            else { model with TouchStartPoint = None
+                              TouchMovingPoint = None
+                              TouchTime = None }, Cmd.none
+        | _ -> { model with TouchMovingPoint = None
+                            TouchTime = None }, Cmd.none
+    | Pause -> { model with IsPaused = true; }, Cmd.none
     | Continue -> { model with IsPaused = false }, Cmd.none
     | HideDetail -> { model with HideDetail = not model.HideDetail }, Cmd.none
 
+let updateCanvas squareSize model =
+    let squareBorder = 1
+    let backgroundColor = "#efe9dc"
+    let borderColor = "rgba(0, 0, 0, 0.3)"
+    let generateColorString color =
+        let r, g, b, a = color
+        sprintf "rgba(%d, %d, %d, %f)" r g b a
+    let canvases = Browser.document.getElementsByTagName_canvas()
+    if canvases.length > 0. then
+        let canvas = canvases.[0]
+        let rows, columns = fst model.Boundry + 1, snd model.Boundry + 1
+        let screenWidth, screenHeight = columns * squareSize, rows * squareSize
+
+        let context = canvas.getContext_2d()
+        context.fillStyle <- !^backgroundColor
+        context.fillRect(0., 0., float screenWidth, float screenHeight)
+
+        let drawSquare location color (context: CanvasRenderingContext2D) =
+            let row, column = location
+            let x, y = squareSize * column, squareSize * row
+            context.fillStyle <- !^borderColor
+            context.fillRect(float x, float y, float squareSize, float squareSize)
+            context.fillStyle <- !^color
+            context.fillRect(float (x + squareBorder), float (y + squareBorder), float (squareSize - squareBorder * 2), float (squareSize - squareBorder * 2))
+        let drawGrid targetContext =
+            for r in 0..rows do
+                for c in 0..columns do
+                    drawSquare (r, c) backgroundColor targetContext
+            targetContext
+        let drawBlock block targetContext =
+            match block with
+            | Some mb ->
+                for square in mb.Squares do drawSquare square.Location (generateColorString square.Color) targetContext
+                targetContext
+            | _ -> targetContext
+    
+        context
+        |> drawGrid
+        |> drawBlock model.PreviewBlock
+        |> drawBlock model.MovingBlock
+        |> drawBlock model.PrectionBlock
+        |> fun x ->
+            model.AllSquares |> List.iter (fun s -> drawSquare s.Location "rgba(0,0,0,0.8)" x)
+            
 let view (model : Model) (dispatch : Msg -> unit) =
     let generateColorString color =
         let r, g, b, a = color
         sprintf "rgba(%d, %d, %d, %f)" r g b a
-    let squareSize = 15
+    let squareSize = 18
     let pixleLength length = sprintf "%dpx" length
-    let createSquare row column color =
-        div [ Class "square"
-              Style [
-                BackgroundColor (generateColorString color)
-                Width (pixleLength squareSize); Height (pixleLength squareSize)
-                Position "absolute"
-                Top (pixleLength (15 * row)); CSSProp.Left (pixleLength (15 * column))
-            ]] []
     Browser.window.location.href <- "#root" // Focus root on init
+    updateCanvas squareSize model
     div [ Class "root"; Id "root"
           TabIndex 0.
           OnClick (fun e ->
             (e.target :?> Browser.HTMLElement).focus()
-            Rotate |> Msg.Action |> dispatch)
+            //Rotate |> Msg.Action |> dispatch
+            )
           OnTouchStart (fun e -> (e.changedTouches.[0.].clientX, e.changedTouches.[0.].clientY) |> TouchStart |> dispatch)
-          OnTouchEnd (fun e ->
-            match model.TouchStartPos with
-            | Some (x0, y0) -> 
-                let x, y = e.changedTouches.[0.].clientX, e.changedTouches.[0.].clientY
-                let dx, dy = x - x0, y - y0
-                let threshhold = 40.
-                let extraSteps d =
-                    (Math.abs d) - threshhold * 3.
-                    |> fun x -> if x > 0. then int (x / 6.) else 0
-                    |> fun x -> if x > 5 then 5 else x
-                if Math.abs dx > Math.abs dy && Math.abs dx > threshhold then
-                    if dx > 0. then for _ in 0..(extraSteps dx) do Right |> Msg.Action |> dispatch
-                    else for _ in 0..(extraSteps dx) do Left |> Msg.Action |> dispatch
-                if Math.abs dx < Math.abs dy && dy > threshhold then
-                    for _ in 0..(extraSteps dy) do Down |> Msg.Action |> dispatch
-            | _ -> ()
-            TouchEnd |> dispatch) 
+          OnTouchMove (fun e -> (e.changedTouches.[0.].clientX, e.changedTouches.[0.].clientY) |> TouchMove |> dispatch)
+          OnTouchEnd (fun e -> (e.changedTouches.[0.].clientX, e.changedTouches.[0.].clientY) |> TouchEnd |> dispatch)
           OnKeyDown (fun e ->
             if e.keyCode = 37. then Left |> Msg.Action |> dispatch
             elif e.keyCode = 38. then Rotate |> Msg.Action |> dispatch
@@ -159,26 +240,14 @@ let view (model : Model) (dispatch : Msg -> unit) =
             if not model.IsRestarting then
                 yield div [ Class "container" ]
                     [
-                        yield div [ Class "playground";
-                              Style [ Width (pixleLength ((snd model.Boundry + 1) * squareSize))
-                                      Height (pixleLength ((fst model.Boundry + 1) * squareSize)) ] ]
+                        yield canvas [ Class "playground"
+                                       HTMLAttr.Width ((snd model.Boundry + 1) * squareSize)
+                                       HTMLAttr.Height ((fst model.Boundry + 1) * squareSize) ]
+                            []
+                        yield div [ Class "center-h info score"] 
                             [
-                                for row in 0..(fst model.Boundry) do
-                                    for col in 0..(snd model.Boundry) ->
-                                        createSquare row col (255, 255, 255, 0.)
-                                for block in model.AllSquares -> 
-                                    createSquare (fst block.Location) (snd block.Location) (0, 0, 0, 1.)
-                                if model.PreviewBlock.IsSome && model.MovingBlock.IsSome &&
-                                   not (model.MovingBlock.Value.Squares |> List.exists (fun x -> fst x.Location < 5)) then
-                                    for block in model.PreviewBlock.Value.Squares ->
-                                        let r, g, b, a = block.Color
-                                        createSquare (fst block.Location) (snd block.Location) (r, g, b, 0.2)
-                                if model.MovingBlock.IsSome then
-                                    for block in model.MovingBlock.Value.Squares ->
-                                        createSquare (fst block.Location) (snd block.Location) block.Color
-                                if model.PrectionBlock.IsSome then
-                                    for block in model.PrectionBlock.Value.Squares ->
-                                        createSquare (fst block.Location) (snd block.Location) block.Color
+                                div [] [str (sprintf "分数：%d" (model.Score))]
+                                div [] [str (sprintf " 🙏 %s 秒" (string (model.TimeCost / 10)))]
                             ]
                         yield div [ Class "controls" ]
                             [
@@ -202,11 +271,6 @@ let view (model : Model) (dispatch : Msg -> unit) =
                                     Button.OnClick (fun e -> e.stopPropagation(); Continue |> dispatch) ]
                                     [ Icon.faIcon [ Icon.Size IsLarge ] [ Fa.icon Fa.I.Forward ]
                                       span [] [str "继续"] ]
-                            ]
-                        yield div [ Class "center-h info score"] 
-                            [
-                                div [] [str (sprintf "分数：%d" (model.Score))]
-                                div [] [str (sprintf " 🙏 %s 秒" (string (model.TimeCost / 10)))]
                             ]
                         if not model.HideDetail then 
                             yield div [ Class "center-v info" ]
@@ -235,7 +299,7 @@ let view (model : Model) (dispatch : Msg -> unit) =
                             ]
                     ]
         ]
-        
+
 let timerTick dispatch = 
     let timer = new System.Timers.Timer(100.0)
     timer.Elapsed.Subscribe (fun _ -> dispatch Tick) |> ignore
@@ -255,6 +319,6 @@ Program.mkProgram init update view
 #endif
 |> Program.withReact "elmish-app"
 #if DEBUG
-|> Program.withDebugger
+//|> Program.withDebugger
 #endif
 |> Program.run
