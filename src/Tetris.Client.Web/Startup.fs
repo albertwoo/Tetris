b3f8185e716e9e0158e665689f53f5f9a895bf90
slaveoftime <albertwoo_slaveoftime@hotmail.com>
2021-02-17 14:53:10 +0800

    Add dev proxy

--- src/Tetris.Client.Web/Startup.fs
+++ src/Tetris.Client.Web/Startup.fs
@@ -4,10 +4,6 @@ open Elmish
 open Elmish.React
 open Fable.Core.JsInterop
 
-#if DEBUG
-// open Elmish.Debug
-open Elmish.HMR
-#endif
 
 importAll "@babel/polyfill"
 importAll "core-js"
@@ -16,8 +12,5 @@ Program.mkProgram App.States.init App.States.update App.Views.render
 #if DEBUG
 |> Program.withConsoleTrace
 #endif
-|> Program.withReactBatched "root"
-#if DEBUG
-//|> Program.withDebugger
-#endif
+|> Program.withReactSynchronous "root"
 |> Program.run
